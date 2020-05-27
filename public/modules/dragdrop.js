23e008fe095176e9b72748484af9107b07ac94b5
ebarashkin <ebarashkin@gmail.com>
2020-05-27 09:35:23 +0300

    update 27/5

--- public/modules/dragdrop.js
+++ public/modules/dragdrop.js
@@ -10,6 +10,7 @@ function dragAndDrop() {
     let dragElement = event.target.closest('#circle'); // Определяем элемент, по которому произвели клик
 
     if (!dragElement) return; // Если элемента нет, то начинаем заново
+    
     const dragElemInArray = f.find(
       (c) => `circle-${c.id}` === dragElement.className
     );
@@ -24,6 +25,7 @@ function dragAndDrop() {
 
       let newX = pageX - circleRadius / 2 - coordWrap.x;
       let newY = pageY - circleRadius / 2 - coordWrap.y;
+
       let newR = coordWrap.right - circleRadius - coordWrap.x - 2;
       let newB = coordWrap.bottom - circleRadius - coordWrap.y - 2;
 
@@ -61,11 +63,11 @@ function dragAndDrop() {
         }
 
         if (testCollision(crcle)) {
-          console.log('пересечение', crcle);
-          document.querySelector(`.circle-${crcle.id}`).hidden = true;
+          //console.log('пересечение', crcle);
+          document.querySelector(`.circle-${crcle.id}`).style.visibility = 'hidden'
         } else {
-          console.log('не пересечение', crcle);
-          document.querySelector(`.circle-${crcle.id}`).hidden = false;
+          //console.log('не пересечение', crcle);
+          document.querySelector(`.circle-${crcle.id}`).style.visibility = '';
         }
       });
     }
@@ -88,11 +90,25 @@ function dragAndDrop() {
         .getBoundingClientRect();
       let j = dragElement.getBoundingClientRect();
 
+      let jX = j.x - coordWrap.x - 1 + j.width/2
+      let jY = j.y - coordWrap.y - 1 + j.width/2
+
+      let iX = i.x - coordWrap.x - 1 + i.width/2
+      let iY = i.y - coordWrap.y - 1 + i.width/2
+
+      console.log(Math.abs(jX - iX));
+      
+
+
+
       return (
-        Math.round(i.top) + i.height > Math.round(j.top) &&
-        Math.round(i.left) + i.width > Math.round(j.left) &&
-        Math.round(i.bottom) - i.height < Math.round(j.bottom) &&
-        Math.round(i.right) - i.width < Math.round(j.right)
+        Math.abs(jX - iX) <= i.width/2 + i.width/2 &&
+        Math.abs(jY - iY) <= i.width/2 + i.width/2 
+
+        //Math.round(i.top) + i.height > Math.round(j.top) &&
+        //Math.round(i.left) + i.width > Math.round(j.left) &&
+        //Math.round(i.bottom) - i.height < Math.round(j.bottom) &&
+        //Math.round(i.right) - i.width < Math.round(j.right)
       );
     }
   });
